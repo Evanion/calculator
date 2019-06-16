3e20dacffe2f87b262085fd151de3e7e9e0a28e2
Mikael Pettersson <evanion@icloud.com>
2019-06-16 17:20:50 +0200

    #1 add global styles, tslint, prettier

---
+++ src/Styles/Theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  textFontFamily: 'sans-serif',
+  textColor: '#000',
+  appBackground: 'linear-gradient(0deg, #FAFBFB, #B9C7D8)',
+  contentBackground: {
+    base: '#8DCDF9',
+    primary: '#F4C2CA',
+  },
+};
